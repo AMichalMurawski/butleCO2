a18617bd61e76784b27a247800b54f3ccd2b452c
Michal M <m.a.murawski@wp.pl>
2025-04-10 23:04:43 +0200

    add About section

---
+++ src/sections/About/About.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AboutSection } from './About.styled';
+
+const About: React.FC = () => {
+  return <AboutSection>Section Hero</AboutSection>;
+};
+
+export default About;
--- src/sections/index.tsx
+++ src/sections/index.tsx
@@ -1,5 +1,6 @@
-export { default as Contact } from "./Contact/Contact";
-export { default as Hero } from "./Hero/Hero";
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as Orders } from "./Orders/Orders";
-export { default as Services } from "./Services/Services";
+export { default as About } from './About/About';
+export { default as Contact } from './Contact/Contact';
+export { default as Hero } from './Hero/Hero';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Orders } from './Orders/Orders';
+export { default as Services } from './Services/Services';
