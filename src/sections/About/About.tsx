6aaabdf14b2a75b0b6015ea6b4fc8ce34e1ece92
Michał Murawski <m.a.murawski@wp.pl>
2025-04-14 15:26:44 +0200

    rearrangement positions

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -6,29 +6,33 @@ import {
   AdvantagesList,
   Description,
   IconWraper,
+  TextBlock,
   Title,
 } from './About.styled';
 import IconSvg from '../../components/Icon/IconSvg';
 
 const advantagesIcons = [
-  ['truck', 'Szybka dostawa pod drzwi'],
-  ['user-check', 'Obsługa klientów indywidualnych i firm'],
-  ['calendar', 'Elastyczne zamówienia'],
-  ['bubbles', 'Fachowe doradztwo'],
-  ['checkmark', 'Bezpieczne i certyfikowane produkty'],
+  ['clock', 'Szybkość'],
+  ['coin-dollar', 'Cena'],
+  ['stats-dots', 'Doświadczenie'],
+  ['bubbles',"Obsługa"],
+  ['user-check', 'Zaufanie'],
+  ['checkmark', 'Bezpieczeństwo'],
 ];
 
 const About: React.FC = () => {
   return (
     <AboutSection>
-      <Title>Poznaj ButleCO2.pl - Ekspertów od CO2 w Warszawie.</Title>
-      <Description>
-        Od ponad 15 lat dostarczamy butle z dwutlenkiem węgla na terenie Warszawy i okolic. Nasza
-        firma powstała z pasji do wspierania lokalnych biznesów - od restauracji, biur i kawiarni po
-        warsztaty przemysłowe i hobbystów akwarystyki. Stawiamy na niezawodność, elastyczność i
-        pełne zadowolenie klienta, co potwierdza stale rosnąca liczba naszych partnerów. Nasz cel?
-        Dostarczać gaz tam, gdzie jest potrzebny - szybko i bez zbędnych formalności.
-      </Description>
+      <TextBlock>
+        <Title>Poznaj ButleCO2.pl - Ekspertów od CO2 w Warszawie.</Title>
+        <Description>
+          Od ponad 15 lat dostarczamy butle z dwutlenkiem węgla na terenie Warszawy i okolic. Nasza
+          firma powstała z pasji do wspierania lokalnych biznesów - od restauracji, biur i kawiarni po
+          warsztaty przemysłowe i hobbystów akwarystyki. Stawiamy na niezawodność, elastyczność i
+          pełne zadowolenie klienta, co potwierdza stale rosnąca liczba naszych partnerów. Nasz cel?
+          Dostarczać gaz tam, gdzie jest potrzebny - szybko i bez zbędnych formalności.
+        </Description>
+      </TextBlock>
       <AdvantagesList>
         {advantagesIcons.map(advantage => (
           <Advanatage>
