70b768c2d2e051fcbac18d7c4613e9281e0d052d
Michal M <m.a.murawski@wp.pl>
2025-04-16 21:40:21 +0200

    change flex to grid in contacts wraper

--- src/sections/ContactUs/ContactUs.styled.ts
+++ src/sections/ContactUs/ContactUs.styled.ts
@@ -2,36 +2,48 @@ import styled from 'styled-components';
 import { H2, P, Section } from '../../styles/Global.styled';
 
 export const ContactSection = styled(Section)`
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
 `;
 
-export const Title = styled(H2)``
+export const Title = styled(H2)``;
 
 export const ContactsWraper = styled.div`
-    display: flex;
-    flex-direction: row;
-    gap: 30px;
-    justify-content: space-around;
-`
+  display: grid;
+  grid-template-columns: 40% 60%;
+  grid-template-rows: repeat(3, auto);
+  gap: 30px;
+`;
 
 export const AddressWraper = styled.address`
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
-`
+  grid-column: 1;
+  grid-row: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
 
-export const Address = styled(P)``
+export const Address = styled(P)``;
 
-export const AddressLink = styled.a``
+export const AddressLink = styled.a``;
 
 export const WorkHoursConteiner = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
-`
+  grid-column: 1;
+  grid-row: 2;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
 
-export const WorkHours = styled(P)``
+export const WorkHours = styled(P)``;
 
-export const FormWraper = styled.div``
+export const FormWraper = styled.div`
+  grid-column: 2;
+  grid-row: 1 / 3;
+`;
+
+export const MapWraper = styled.div`
+  grid-column: 1 / 3;
+  grid-row: 3;
+`;
