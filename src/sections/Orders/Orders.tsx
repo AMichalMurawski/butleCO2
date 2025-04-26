a7c4a6e2dc3070e11ee9f35c736fa019a63ae2a4
Michal M <m.a.murawski@wp.pl>
2025-04-26 23:12:42 +0200

    add initialValues + types

--- src/sections/Orders/Orders.tsx
+++ src/sections/Orders/Orders.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { OrderSection } from './Orders.styled';
+import { Formik } from 'formik';
+import { initialValues } from './initialValues';
 
 const Orders: React.FC = () => {
-  return <OrderSection id="orders">Order Section</OrderSection>;
+  const handleSubmit = () => {};
+
+  return (
+    <OrderSection id="orders">
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}></Formik>
+    </OrderSection>
+  );
 };
 
 export default Orders;
