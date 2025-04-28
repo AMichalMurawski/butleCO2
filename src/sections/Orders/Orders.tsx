87e81a40644366ebe7207d55c98c9babb9d33b09
Michał Murawski <m.a.murawski@wp.pl>
2025-04-28 16:19:58 +0200

    create invoice header

--- src/sections/Orders/Orders.tsx
+++ src/sections/Orders/Orders.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { OrderSection } from './Orders.styled';
+import { Address, AddressWraper, HeaderTitle, InvoiceAddress, InvoiceHeader, OrderNumber, OrderSection, Title } from './Orders.styled';
 import { Formik } from 'formik';
 import { initialValues } from './initialValues';
 
@@ -8,7 +8,23 @@ const Orders: React.FC = () => {
 
   return (
     <OrderSection id="orders">
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}></Formik>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <InvoiceHeader>
+          <HeaderTitle>
+            <Title>Zamówienie</Title>
+            <OrderNumber>nr: 542455789</OrderNumber>
+          </HeaderTitle>
+          <InvoiceAddress>
+            <AddressWraper>
+              <Address>ButleCO2.PL Artur Rembek</Address>
+              <Address>ul. Poprawna 105A</Address>
+              <Address>03-984 Warszawa</Address>
+              <Address>NIP: 9522030252</Address>
+            </AddressWraper>
+          </InvoiceAddress>
+        </InvoiceHeader>
+
+      </Formik>
     </OrderSection>
   );
 };
