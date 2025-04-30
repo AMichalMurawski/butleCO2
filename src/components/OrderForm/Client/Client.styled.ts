0359a59ca25be39ccb55c7b89cd86039600f43bf
Michal M <m.a.murawski@wp.pl>
2025-04-30 23:20:07 +0200

    add animation to client conteiners

--- src/components/OrderForm/Client/Client.styled.ts
+++ src/components/OrderForm/Client/Client.styled.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { H3, P } from '../../../styles/Global.styled';
 
 interface ClientWraperProps {
@@ -7,7 +7,7 @@ interface ClientWraperProps {
 
 export const ClientWraper = styled.div<ClientWraperProps>`
   position: relative;
-  padding-inline: 60px;
+  margin-inline: 60px;
   ${({ $autoMargin }) => ($autoMargin === 'right' ? 'margin-right' : 'margin-left')}: auto;
   min-width: 50%;
   max-width: 75%;
@@ -15,6 +15,7 @@ export const ClientWraper = styled.div<ClientWraperProps>`
   display: flex;
   flex-direction: column;
   gap: 5px;
+  cursor: pointer;
 `;
 
 export const Subtitle = styled(H3)`
@@ -44,22 +45,35 @@ export const DataValue = styled(P)`
   color: ${({ theme }) => theme.color.main};
 `;
 
+const write = keyframes`
+  0% { transform: transalate(0px, 0px); }
+  10% { transform: translate(-18px, 6px); }
+  25% { transform: translate(-12px, 0px); }
+  35% { transform: translate(-14px, 6px); }
+  50% { transform: translate(-8px, 0px); }
+  60% { transform: translate(-10px, 6px); }
+  75% { transform: translate(-4px, 0px); }
+  85% { transform: translate(-6px, 6px); }
+  100% { transform: translate(0px, 0px); }
+`;
+
 export const EditIconWraper = styled.div`
   position: absolute;
   top: 0;
-  right: 55px;
+  right: 0;
   width: 30px;
   height: 30px;
   border-radius: 100%;
   // background-color: ${({ theme }) => theme.color.remarkable};
-  border: 3px solid ${({ theme }) => theme.color.structural};
+  // border: 3px solid ${({ theme }) => theme.color.structural};
   cursor: pointer;
 
   & svg {
     transform: translate(3px, 2px);
   }
 
-  &:hover {
-    scale: 1.2;
+  ${ClientWraper}:hover & {
+    // scale: 1.1;
+    animation: ${write} 2s ease-in infinite;
   }
 `;
