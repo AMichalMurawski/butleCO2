8b370e1b88a9a331cab9a583ba32db685ac8dbff
Michal M <m.a.murawski@wp.pl>
2025-06-18 01:00:03 +0200

    GoogleReviews

--- src/sections/GoogleReviews/GoogleReviews.styled.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import { H2, Section } from "../../styles/Global.styled";
-
-export const GoogleReviewsSection = styled(Section)``;
-
-export const Title = styled(H2)``
