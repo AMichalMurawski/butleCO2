5f4e3833b3acac0fa3a73b6e71d16ddb8148c6e5
Michał Murawski <m.a.murawski@wp.pl>
2025-07-14 15:26:01 +0200

    Order Header media

--- src/components/Order/Header/Header.styled.ts
+++ src/components/Order/Header/Header.styled.ts
@@ -3,9 +3,15 @@ import { H2, P } from '../../../styles';
 
 export const HeaderWraper = styled.div`
   display: flex;
-  height: 8.5rem;
+  flex-direction: column;
+  height: 12rem;
   width: 100%;
   overflow: hidden;
+
+  ${({ theme }) => theme.media.t} {
+    flex-direction: row;
+    height: 8.5rem;
+  }
 `;
 
 export const HeaderTitle = styled.div`
@@ -16,51 +22,63 @@ export const HeaderTitle = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 2;
-  padding: 1rem 4rem;
+  padding: 1rem 4rem 0;
   width: 100%;
   z-index: 0;
 
-  &::before,
-  &::after {
-    content: '';
-    position: absolute;
-    top: 0%;
-    left: 0%;
-    width: calc(100% + 2rem);
-    height: 100%;
-    background-color: ${({ theme }) => theme.color.remarkable};
-  }
+  ${({theme}) => theme.media.t} {
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0%;
+      left: 0%;
+      width: calc(100% + 2rem);
+      height: 100%;
+      background-color: ${({ theme }) => theme.color.remarkable};
+    }
 
-  &::before {
-    clip-path: polygon(
-      calc(100% - 5rem) 0%,
-      100% 0,
-      calc(100% - 5rem) 100%,
-      calc(100% - 10rem) 100%
-    );
-  }
+    &::before {
+      clip-path: polygon(
+        calc(100% - 5rem) 0%,
+        100% 0,
+        calc(100% - 5rem) 100%,
+        calc(100% - 10rem) 100%
+      );
+    }
 
-  &::after {
-    clip-path: polygon(0% 100%, 0 70%, calc(100% - 5rem) 70%, calc(100% - 5rem) 100%);
+    &::after {
+      clip-path: polygon(0% 100%, 0 70%, calc(100% - 5rem) 70%, calc(100% - 5rem) 100%);
+    }
   }
 `;
 
 export const Title = styled(H2)`
-  position: absolute;
-  top: 2.75rem;
-  left: 4rem;
+  position: relative;
   text-align: left;
   color: ${({ theme }) => theme.color.text};
 
   &::before {
     content: '';
     position: absolute;
-    top: -75%;
-    left: -100%;
+    top: -0.5rem;
+    left: -600px;
     width: 1200px;
-    height: 300%;
+    height: 600%;
     background-color: ${({ theme }) => theme.color.structural};
     z-index: -1;
+  }
+
+  ${({ theme }) => theme.media.t} {
+    position: absolute;
+    top: 2.75rem;
+    left: 4rem;
+
+    &::before {
+      top: -75%;
+      left: -100%;
+    height: 300%;
+    }
   }
 `;
 
@@ -83,19 +101,25 @@ export const InvoiceAddress = styled.div`
   gap: 0;
   width: 100%;
   height: 100%;
-  color: ${({ theme }) => theme.color.structural};
+  color: ${({ theme }) => theme.color.text};
   text-align: left;
   z-index: 1;
+  translate: 0 0.4rem;
 
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: -1rem;
-    width: calc(100% + 2rem);
-    height: 100%;
-    background-color: ${({ theme }) => theme.color.remarkable};
-    clip-path: polygon(5rem 0%, 100% 0%, 100% 100%, 0 100%);
-    z-index: -1;
+  ${({ theme }) => theme.media.t} {
+    color: ${({ theme }) => theme.color.structural};
+  translate: 0 0;
+  
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: -1rem;
+      width: calc(100% + 2rem);
+      height: 100%;
+      background-color: ${({ theme }) => theme.color.remarkable};
+      clip-path: polygon(5rem 0%, 100% 0%, 100% 100%, 0 100%);
+      z-index: -1;
+    }
   }
 `;
